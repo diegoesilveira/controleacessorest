6fde0090081207f1e2ac6c8d9d3a4d3c4048b714
Diego Silveira <diego.silveira@gx2.com.br>
2020-02-03 11:05:52 -0300

    Criado os EndPoints PermissaoUsuarioResource e UsuarioResource.

---
+++ src/main/java/br/com/gx2/resources/UsuarioResource.java
@@ -0,0 +1,10 @@
+package br.com.gx2.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/usuarios")
+public class UsuarioResource {
+
+}
