d0b30435660c6e85e72ef514b3458009ec4e3842
Diego Silveira <diego.silveira@gx2.com.br>
2020-02-03 09:10:00 -0300

    Criando projeto controle de acesso rest

---
+++ src/main/java/br/com/gx2/ControleAcessoRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.gx2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleAcessoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleAcessoRestApplication.class, args);
+	}
+
+}
